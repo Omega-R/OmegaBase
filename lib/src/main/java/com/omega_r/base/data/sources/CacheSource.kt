a3c416a413795108420477e95c63be1e676cb4f6
Anton Knyazev <anton.knyazev@omega-r.com>
2019-07-15 12:02:58 +0300

    Fixed cache source

--- lib/src/main/java/com/omega_r/base/data/sources/CacheSource.kt
+++ lib/src/main/java/com/omega_r/base/data/sources/CacheSource.kt
@@ -3,9 +3,19 @@ package com.omega_r.base.data.sources
 /**
  * Created by Anton Knyazev on 12.04.19.
  */
-interface CacheSource: Source {
+interface CacheSource : Source {
 
-    fun update(data: Any?)
+    fun updateItem(data: Any?)
+
+    fun updateItems(data: List<*>?) = data?.forEach { updateItem(it) }
+
+    fun update(data: Any?) {
+        if (data is List<*>) {
+            updateItems(data)
+        } else {
+            updateItem(data)
+        }
+    }
 
     fun clear()
 
